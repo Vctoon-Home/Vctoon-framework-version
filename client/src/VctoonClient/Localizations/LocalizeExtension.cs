4542df0edd2bfdcb2a81d55aff179af9c1c62c4f
zyknow <1047606598@qq.com>
2023-08-01 09:57:54 +0800

    remove LocalizeExtension note

--- client/src/VctoonClient/Localizations/LocalizeExtension.cs
+++ client/src/VctoonClient/Localizations/LocalizeExtension.cs
@@ -33,33 +33,4 @@ public class LocalizeExtension : MarkupExtension
 
         return binding;
     }
-    //
-    // public override object ProvideValue(IServiceProvider serviceProvider)
-    // {
-    //     var manager = App.Services.GetService<LocalizationManager>();
-    //
-    //     // 强制触发 MultiBinding 的更新，即使我们实际上并不需要这个属性的值
-    //     var dummyBinding = new Binding("CurrentCulture")
-    //     {
-    //         Mode = BindingMode.OneWay,
-    //         Source = manager
-    //     };
-    //
-    //     var localizationBinding = new Binding($"[{Text}]")
-    //     {
-    //         Mode = BindingMode.OneWay,
-    //         Source = manager,
-    //         StringFormat = StringFormat
-    //     };
-    //
-    //     return new MultiBinding
-    //     {
-    //         Bindings =
-    //         {
-    //             dummyBinding,
-    //             localizationBinding
-    //         },
-    //         Converter = new LocalizationConverter()
-    //     };
-    // }
 }
